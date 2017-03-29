b679356dd058dfd7a04221fe8724a6a33a482c47
e <16126240@bjtu.edu.cn>
2017-03-30 01:21:52 +0800

    项目已提交
    git commit -m 项目已提交

---
+++ ShootDemo/Assets/Standard Assets/ParticleSystems/Scripts/Bullet.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+public var thrust: float;
+private var rb: Rigidbody;
+
+function Start () {
+	rb = GetComponent.<Rigidbody>();
+	Destroy(gameObject,5.0);
+	
+}
+
+function FixedUpdate () {
+  rb.AddForce(transform.forward * thrust);
+}
---
+++ ShootDemo/Assets/Standard Assets/ParticleSystems/Scripts/ShootOrder.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+public var moveSpeed: float;
+public var bullet: GameObject;
+public var bulletNember: int = 0;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	var x: float = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;  
+	var z: float = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
+
+	//移动
+    transform.Translate(x,0,z);
+    //开火
+    if (Input.GetButtonDown("Fire1")) {
+    	Instantiate(bullet,transform.position,transform.rotation);
+    	bulletNember++;
+
+    	gameObject.Find("Canvas/BulletsList").GetComponent.<UI.Text>().text
+    	    = "开火数：" + bulletNember;
+    }
+    
+    //仰
+    if (Input.GetKey(KeyCode.Q)) {
+    	print("Q");
+    	transform.Rotate(0,-25*Time.deltaTime,0,Space.Self);
+    }
+    //俯视
+    if (Input.GetKey(KeyCode.E)) {
+    	transform.Rotate(0,25*Time.deltaTime,0,Space.Self);
+    }
+    //左
+    if (Input.GetKey(KeyCode.Z)) {
+    	transform.Rotate(-25*Time.deltaTime,0,0,Space.Self);
+    }
+    //右
+    if (Input.GetKey(KeyCode.C)) {
+    	transform.Rotate(25*Time.deltaTime,0,0,Space.Self);
+    }
+    //升高镜头
+    if (Input.GetKey(KeyCode.H)) {
+    	transform.Translate(0,5*Time.deltaTime,0);
+    }
+    //调低镜头
+    if (Input.GetKey(KeyCode.N)) {
+    	transform.Translate(0,-5*Time.deltaTime,0);
+    }    
+}
